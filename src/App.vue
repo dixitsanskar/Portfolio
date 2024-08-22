e5c501f243409e807692ba803504d8d4f1f23379
dixitsanskar <sanskar.sanjay@gmail.com>
2024-08-22 19:33:00 +0530

    Portfolio init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import HeadBar from './components/Header.vue'
+</script>
+
+<template>
+  <header><HeadBar/></header>
+  
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+
+<template>
+        <header class="d-flex flex-wrap justify-content-between py-3 mb-4">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 link-body-emphasis text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+        <span class="fs-4 text-[#ffd22f] ">SANSKAR</span>
+      </a>
+
+      <ul class="nav nav-pills flex justify-between">
+        <li class="nav-item"><a href="#" class="nav-link" aria-current="page">About</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Skills</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Portfolio</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Testimonial</a></li>
+      </ul>
+
+      <ul class="nav nav-pills">
+      <li class="nav-item"><a href="#" class="nav-link active bg-transparent !text-[#ffd22f] !border-[#ffd22f] border " aria-current="page">Download Resume</a></li>
+
+      </ul>
+
+      
+    </header>
+    
+</template>
+<style scoped>
+.nav-link{
+  color:white;
+}
+</style>
