b0d2c9f76219bbd2721812b17175415d13ccc687
dixitsanskar <sanskar.sanjay@gmail.com>
2024-08-26 15:51:05 +0530

    Number block added

---
+++ src/components/NumberBlock.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+    num:{
+        type: String,
+        required: true,
+    },
+    service:{
+        type: String,
+        required: true
+    }
+})
+</script>
+<template>
+    <div class="parent  h-24  font-poppins">
+      <div class="text-4xl font-semibold  text-yellow-400">{{num}}</div>
+      <div class="mt-4 text-2xl leading-none text-white">{{service}}</div>
+    </div>
+  </template>
---
+++ src/views/number.vue
@@ -0,0 +1,11 @@
+<script setup>
+import comp from '@/components/NumberBlock.vue'
+</script>
+<template>
+    <div class="flex  flex-row  justify-center">
+    <comp class="  border-r border-yellow-400 px-[135px]  " num="30+" service="Satisfied Client" />
+    <comp class="border-r border-yellow-400 px-[135px] " num="20+" service="Projects Completed" />
+    <comp class="px-[135px]" num="40+" service="Reviews Given" />
+    </div>
+
+</template>
